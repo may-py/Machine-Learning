10b881f4aa35467c84cf1f73841f5eff9e5eccd0
Mayur Suthar <mayur1601@yahoo.in>
2023-08-14 10:26:51 +0700

    Flask and PRediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,27 @@
 import os
 import sys
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 import dill
-
-from src.exception import CustomException
-from src.logger import logging
-from .components import data_transformation
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
-
-
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj,file_obj)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise(CustomException(e,sys))
+        raise CustomException(e, sys)
     
-
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -39,6 +36,8 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
@@ -50,6 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
